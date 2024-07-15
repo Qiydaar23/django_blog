f813c6537e4f3d5dabfe8514cb31becac86546b1
Qiydaarwilliams23@gmail.com <Qiydaarwilliams23@gmail.com>
2024-07-15 16:35:55 -0400

    user/login made added redirect to settings

--- hub_app/settings.py
+++ hub_app/settings.py
@@ -124,7 +124,11 @@ STATIC_URL = 'static/'
 
 CRISPY_TEMPLATE_PACK ='bootstrap4'
 
+LOGIN_REDIRECT_URL = 'blog-home'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
